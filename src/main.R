78767cf451ee961d82fec671712d9ee2f08dd8de
Cristiano Ricci <cristiano.ricci6@gmail.com>
2023-11-03 14:33:16 +0100

    addded dataset

--- src/main.R
+++ src/main.R
@@ -7,8 +7,8 @@ folderName = "../datasets_it/"
 
 hA_range = seq(from=5,to=20,by=5)
 hR_range = seq(from=20,to=100,by=5)
-hA = 10
-hR = 30
+# hA = 10
+# hR = 30
 
 rawData = create_df(folderName)
 data = rawData$data
@@ -19,9 +19,13 @@ shp_data = rawData$shp_data
 # data$Latitude = df.tot$Latitude.old
 # data$Longitude = df.tot$Longitude.old
 
-# bestIV_est = chose_hAhR(data,hA_range,hR_range)
+bestIV_est = chose_hAhR(data,hA_range,hR_range)
+hA = bestIV_est$hABest
+hR = bestIV_est$hRBest
+
 # outIVEstimate = estimate_IV_SARD_auto(data,hA,hR)
 # outWNEstimate = estimate_WN_SARD_auto(data,hA,hR)
+
 outWNEstimate = estimate_SARD_auto(data,shp_data,hA,hR)
 
 
